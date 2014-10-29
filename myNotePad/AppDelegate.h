692d2dab2a61d81c8b4301f51f54fcbd2eb122cf
hezt95 <hezt95@gmail.com>
2014-10-29 18:26:47 +0800

    Initial Commit

---
+++ myNotePad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myNotePad
+//
+//  Created by He Stone on 14/10/29.
+//  Copyright (c) 2014年 ZitongHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myNotePad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myNotePad
+//
+//  Created by He Stone on 14/10/29.
+//  Copyright (c) 2014年 ZitongHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
